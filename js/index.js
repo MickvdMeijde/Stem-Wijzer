ee49a0163a8c9e6ae5d13fcd3bea0193a7c2a590
Mick van der Meijde <mickvdmeijde@gmail.com>
2019-02-13 14:17:28 +0100

    adding JS
    editing index and style

---
+++ js/index.js
@@ -0,0 +1,58 @@
+const title = document.getElementById("questionTitle");
+const description = document.getElementById("questionDescription");
+const agree = document.getElementById("agree");
+var back = document.getElementById("backButton")
+const neither = document.getElementById("neither");
+const disagree = document.getElementById("disagree");
+const page = document.getElementById("questionPage");
+const priority = document.getElementById("priorityPage")
+
+var index = 0;
+
+/* questions*/
+showQuestion();
+
+/*show the question*/
+function showQuestion () {
+	if (index > 0) {
+		back.style.display= "inline-block";
+	}
+	else {
+		back.style.display= "none";
+	}
+	page.style.display= "block";
+	title.innerHTML= (index+1)+". "+subjects[index].title;
+	description.innerHTML= subjects[index].statement;
+}
+
+/*go to next question*/
+function nextQuestion () {
+	index++;
+	/*if the end is reached show the last page*/
+	if (index >= subjects.length) {
+		showPriority();
+	}
+	else{
+		showQuestion();
+	}
+}
+
+/*show the last page*/
+function showPriority () {
+	page.style.display= "none";
+	priority.style.display= "block";
+	generatePriorityList();
+}
+
+/*go to the previous question*/
+function previousQuestion () {
+	index--;
+	showQuestion();
+}
+
+/**/
+function generatePriorityList() {
+	for (var i = 0; i < subjects.length; i++) {
+		console.log(subjects[i].title);
+	}
+}
