94fda5257b6e12affe3cf5af60b102f63c4656bc
Mick van der Meijde <mickvdmeijde@gmail.com>
2019-02-15 12:00:16 +0100

    formatted JS

--- js/index.js
+++ js/index.js
@@ -1,12 +1,12 @@
 const title = document.getElementById("questionTitle");
 const description = document.getElementById("questionDescription");
 const agree = document.getElementById("agree");
-const back = document.getElementById("backButton")
+const back = document.getElementById("backButton");
 const neither = document.getElementById("neither");
 const disagree = document.getElementById("disagree");
 const page = document.getElementById("questionPage");
-const priority = document.getElementById("priorityPage")
-const priorityList = document.getElementById("priorityList")
+const priority = document.getElementById("priorityPage");
+const priorityList = document.getElementById("priorityList");
 
 var index = 0;
 
@@ -14,50 +14,55 @@ var index = 0;
 showQuestion();
 
 /*show the question*/
-function showQuestion () {
+function showQuestion() {
 	if (index > 0) {
-		back.style.display= "inline-block";
+		back.style.display = "inline-block";
+	} else {
+		back.style.display = "none";
 	}
-	else {
-		back.style.display= "none";
-	}
-	page.style.display= "block";
-	title.innerHTML= (index+1)+". "+subjects[index].title;
-	description.innerHTML= subjects[index].statement;
+	page.style.display = "block";
+	title.innerHTML = index + 1 + ". " + subjects[index].title;
+	description.innerHTML = subjects[index].statement;
 }
 
 /*go to next question*/
-function nextQuestion () {
+function nextQuestion() {
 	index++;
 	/*if the end is reached show the last page*/
 	if (index >= subjects.length) {
 		showPriority();
-	}
-	else{
+	} else {
 		showQuestion();
 	}
 }
 
 /*show the last page*/
-function showPriority () {
-	page.style.display= "none";
-	priority.style.display= "block";
+function showPriority() {
+	page.style.display = "none";
+	priority.style.display = "block";
 	/*
 	generatePriorityList();
 	*/
 }
 
 /*go to the previous question*/
-function previousQuestion () {
+function previousQuestion() {
 	index--;
 	showQuestion();
 }
 
-/* */
+/*generate list for end page*/
 function generatePriorityList() {
-	priorityList.innerHTML="";
-	for (var i = 0; i < subjects.length; i++) {
+	priorityList.innerHTML = "";
+	for (let i = 0; i < subjects.length; i++) {
 		console.log(subjects[i].title);
-		priorityList.innerHTML+=subjects[i].title;
+		priorityList.innerHTML += subjects[i].title;
 	}
 }
+
+var answers = [
+	
+	
+];
+
+console.log(parties);
